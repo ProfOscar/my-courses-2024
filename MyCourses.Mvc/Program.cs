1680c0081c6a5620e1d680f7900474f3e3b4f506
Oscar Cambieri <oscar@cambieri.it>
2023-12-13 09:22:09 +0100

    Costruttore primario su Course model

--- MyCourses.Mvc/Program.cs
+++ MyCourses.Mvc/Program.cs
@@ -1,6 +1,9 @@
+using MyCourses.Models.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+Course c = new Course(1, "Test Course", "Simple test for project cross reference", "");
+app.MapGet("/", () => c.ToString());
 
 app.Run();
