3c655040a3aa46d8ddc4fbcc3e7ff6f1698bddea
Oscar Cambieri <oscar@cambieri.it>
2023-12-13 09:37:39 +0100

    Aggiunto documento di specifica e supporto per i file statici

--- MyCourses.Mvc/Program.cs
+++ MyCourses.Mvc/Program.cs
@@ -3,7 +3,9 @@ using MyCourses.Models.Entities;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-Course c = new Course(1, "Test Course", "Simple test for project cross reference", "");
+app.UseStaticFiles();
+
+Course c = new(1, "Test Course", "Simple test for project cross reference", "");
 app.MapGet("/", () => c.ToString());
 
 app.Run();
