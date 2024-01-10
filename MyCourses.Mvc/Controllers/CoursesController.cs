9ffc79bf3ec226fa657d531278cd40c846303d2c
Oscar Cambieri <oscar@cambieri.it>
2024-01-10 08:43:09 +0100

    Aggiunto CoursesController e testato cross reference fra progetti

---
+++ MyCourses.Mvc/Controllers/CoursesController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using MyCourses.Models.Entities;
+
+namespace MyCourses.Mvc.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Sono Index di Courses");
+        }
+
+        public IActionResult Detail(string id)
+        {
+            Course c = new Course(int.Parse(id), "Test Course " + id, "Description for course " + id, "");
+            return Content($"Sono Detail di Courses: {c}");
+        }
+    }
+}
