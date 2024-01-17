edb194a554a6a21453774c2770c22a9da3e850ab
Oscar Cambieri <oscar@cambieri.it>
2024-01-17 11:23:06 +0100

    Aggiunto CourseViewModel specializzata per elenco corsi

--- MyCourses.Models/Services/Application/CourseService.cs
+++ MyCourses.Models/Services/Application/CourseService.cs
@@ -1,26 +1,25 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using MyCourses.Models.Entities;
+using MyCourses.Models.ViewModels;
 
 namespace MyCourses.Models.Services.Application
 {
     public class CourseService
     {
-        public List<Course> GetCourses(){
-            var courseList = new List<Course>();
+        public List<CourseViewModel> GetCourses(){
+            var courseList = new List<CourseViewModel>();
             var rand = new Random();
             for (int i = 0; i <= 20; i++)
             {
                 var price = Convert.ToDecimal(rand.NextDouble() * 10 + 10);
-                var course = new Course
-                (
-                    i,
-                    $"Corso {i}",
-                    $"Description {i}",
-                    "/logo.svg"
-                );
+                var course = new CourseViewModel
+                {
+                    Id = i,
+                    Title = $"Corso {i}",
+                    ImagePath = "/logo.svg",
+                    Author = "Nome Cognome",
+                    Rating = rand.Next(10, 50) / 10.0,
+                    FullPrice = rand.NextDouble() > 0.5 ? price : price + 1,
+                    CurrentPrice = price
+                };
                 courseList.Add(course);
             }
             return courseList;
---
+++ MyCourses.Models/ViewModels/CourseViewModel.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCourses.Models.ViewModels
+{
+    public class CourseViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ImagePath { get; set; }
+        public string Author { get; set; }
+        public double Rating { get; set; }
+        public Decimal FullPrice { get; set; }
+        public Decimal CurrentPrice { get; set; }
+    }
+}
