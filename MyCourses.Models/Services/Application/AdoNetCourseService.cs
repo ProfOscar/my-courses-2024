741bd5efd177f5102be6f1b9622d5990b1e4fa4a
Oscar Cambieri <oscar@cambieri.it>
2024-02-07 09:30:09 +0100

    Migliorato l'organizzazione del codice per il parsing di oggetti CourseViewModel da dati provenienti dal DB

--- MyCourses.Models/Services/Application/AdoNetCourseService.cs
+++ MyCourses.Models/Services/Application/AdoNetCourseService.cs
@@ -1,7 +1,5 @@
 using System.Data;
-using MyCourses.Models.Enums;
 using MyCourses.Models.Services.Infrastructure;
-using MyCourses.Models.ValueObjects;
 using MyCourses.Models.ViewModels;
 
 namespace MyCourses.Models.Services.Application
@@ -22,26 +20,10 @@ namespace MyCourses.Models.Services.Application
             var courseList = new List<CourseViewModel>();
             foreach (DataRow courseRow in dataTable.Rows)
             {
-                CourseViewModel course = new CourseViewModel
-                {
-                    Title = Convert.ToString(courseRow["Title"])!,
-                    ImagePath = Convert.ToString(courseRow["ImagePath"])!,
-                    Author = Convert.ToString(courseRow["Author"])!,
-                    Rating = Convert.ToDouble(courseRow["Rating"]),
-                    FullPrice = new Money(
-                        Enum.Parse<Currency>(Convert.ToString(courseRow["FullPrice_Currency"])!),
-                        Convert.ToDecimal(courseRow["FullPrice_Amount"])
-                    ),
-                    CurrentPrice = new Money(
-                        Enum.Parse<Currency>(Convert.ToString(courseRow["CurrentPrice_Currency"])!),
-                        Convert.ToDecimal(courseRow["CurrentPrice_Amount"])
-                    ),
-                    Id = Convert.ToInt32(courseRow["Id"])
-                };
+                CourseViewModel course = CourseViewModel.FromDataRow(courseRow);
                 courseList.Add(course);
             }
             return courseList;
-
         }
 
         public CourseDetailViewModel GetCourse(int id)
